a59ea70e0dbc3f0cd617b800b34938940c4e17a8
Ryan Theodore The <ryan.the.2006@gmail.com>
2021-07-02 21:03:41 +0800

    Initial Commit

---
+++ Jabs/JabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JabsApp.swift
+//  Jabs
+//
+//  Created by Ryan The on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct JabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
